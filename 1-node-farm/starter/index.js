98931889ce57de36c7ea29151e90d30455face47
Jinnn01 <60898623+Jinnn01@users.noreply.github.com>
2023-11-15 17:08:46 +1100

    prettier format

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -2,38 +2,38 @@
 const fs = require('fs');
 // Blocking
 // Read file
-const textIn = fs.readFileSync('./txt/input.txt', 'utf-8')
+const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
 // console.log(textIn);
 
 // Write file
 const textOut = `This is what we know about the avocade:${textIn}.\nCreated on ${Date.now()}`;
 //write into a new file
 fs.writeFileSync('./txt/output.txt', textOut);
-console.log('File written!')
+console.log('File written!');
 
 fs.readFile('./txt/input.txt', 'utf-8', (err, data) => {
-    console.log(data);
+  console.log(data);
 });
 console.log('Reading file...');
 
 //Non-blocking, asynchronous way
 fs.readFile('./txt/start.txt', 'utf-8', (err, data) => {
-    console.log(data);
+  console.log(data);
 });
 console.log('File Reading...');
 
 // read the content of files in start.txt
 fs.readFile('./txt/startt.txt', 'utf-8', (err, filename) => {
-    if (err) return console.log("Oppps");
+  if (err) return console.log('Oppps');
 
-    fs.readFile(`./txt/${filename}.txt`, 'utf-8', (err, data1) => {
-        console.log(data1);
-        fs.readFile('./txt/append.txt', 'utf-8', (err, data2) => {
-            console.log(data2);
-            fs.writeFile('./txt/final2.txt', `${data1}\n${data2}`, 'utf-8', err => {
-                console.log("Your file has been written");
-            });
-        });
+  fs.readFile(`./txt/${filename}.txt`, 'utf-8', (err, data1) => {
+    console.log(data1);
+    fs.readFile('./txt/append.txt', 'utf-8', (err, data2) => {
+      console.log(data2);
+      fs.writeFile('./txt/final2.txt', `${data1}\n${data2}`, 'utf-8', (err) => {
+        console.log('Your file has been written');
+      });
     });
+  });
 });
 console.log('File Reading...');
